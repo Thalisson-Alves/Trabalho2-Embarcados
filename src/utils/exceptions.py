9ff62af579f2a0562b767aeab75959115d65129f
Thalisson-Alves <thalisson2030@gmail.com>
2023-01-22 07:35:53 -0300

    add retries

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -10,12 +10,13 @@ class ModbusCRCError(RuntimeError):
     ...
 
 
-def try_log(fn):
-    def wrapper(*args, **kwargs):
-        try:
-            return fn(*args, **kwargs)
-        except Exception as e:
-            print(f'Error on {fn.__name__} - {e}')
-    
+def retry_log(fn):
+    def wrapper(*args, retries: int = 2, **kwargs):
+        for _ in range(retries + 1):
+            try:
+                return fn(*args, **kwargs)
+            except Exception as e:
+                print(f'Error on {fn.__name__} - {e}')
+
     wrapper.__name__ = fn.__name__
     return wrapper
